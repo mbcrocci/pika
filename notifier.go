a6c6a3efa864e43252f8ab888e578c50c4a6a14f
Maurizio Crocci <mc@kamae.pt>
2022-03-22 11:08:18 +0000

    Simplifies notifier

--- notifier.go
+++ notifier.go
@@ -25,49 +25,54 @@ func (r *RabbitConnector) StartNotifier(notifier Notifier) error {
 		return err
 	}
 
+	notify(channel, notifier)
+
+	go notifyLoop(channel, notifier)
+
 	options := notifier.Options()
-
-	notify := func() {
-		msg, err := notifier.Notify()
-		if err != nil {
-			log.Error(err)
-			return
-		}
-
-		err = channel.Publish(
-			options.Exchange,
-			options.Topic,
-			false,
-			false,
-			amqp.Publishing{
-				ContentType: "text/plain",
-				Body:        []byte(msg),
-			},
-		)
-		if err != nil {
-			log.Error(err)
-		}
-
-	}
-
-	notify()
-
-	go func() {
-		t := time.NewTicker(options.Interval)
-		defer t.Stop()
-
-		for range t.C {
-			if notifier.Stop() {
-				break
-			}
-
-			notify()
-		}
-	}()
-
 	log.WithFields(log.Fields{
 		"Exchange": options.Exchange,
 		"Topic":    options.Topic,
 	}).Info("Started Notifier")
+
 	return nil
+}
+
+func notify(channel *amqp.Channel, notifier Notifier) {
+	options := notifier.Options()
+
+	msg, err := notifier.Notify()
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	err = channel.Publish(
+		options.Exchange,
+		options.Topic,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(msg),
+		},
+	)
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+func notifyLoop(channel *amqp.Channel, notifier Notifier) {
+	options := notifier.Options()
+
+	t := time.NewTicker(options.Interval)
+	defer t.Stop()
+
+	for range t.C {
+		if notifier.Stop() {
+			break
+		}
+
+		notify(channel, notifier)
+	}
 }
